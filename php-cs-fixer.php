f59f80920141808faa8cf612f77bd56b5606f805
silfek78@gmail.com <silfek78@gmail.com>
2022-09-06 15:47:30 +0300

    modified:   php-cs-fixer.php

--- php-cs-fixer.php
+++ php-cs-fixer.php
@@ -3,9 +3,10 @@
 declare(strict_types=1);
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__.'/');
+    ->in(__DIR__);
 
 $config = new PhpCsFixer\Config();
+
 return $config
     ->setRules([
         '@PSR12' => true,
